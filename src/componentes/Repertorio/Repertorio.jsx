439ec2373fdb1223f3a245988f9d64303ad30e89
Natalia Sethson <nataliasethson@live.com.ar>
2026-01-08 10:09:16 -0300

    componente repertorio

--- src/componentes/Repertorio/Repertorio.jsx
+++ src/componentes/Repertorio/Repertorio.jsx
@@ -12,7 +12,7 @@ const Repertorio = () => {
                 Cátedra de Repertorio.
             </h2>
             <h3 className='textRepertorio'>
-                La Cátedra de Repertorio es dirigida por el pianista Diego López, egresado del Conservatorio Superior de Música Manuel de Falla. Su labor va más allá del acompañamiento: Diego guía a los estudiantes en una comprensión profunda de cada pieza, abordando su contexto histórico, el estilo del compositor y la esencia dramática de la obra.
+                La Cátedra de Repertorio es dirigida por el pianista Diego López, egresado del Conservatorio Superior de Música Manuel de Falla. Su labor va más allá del acompañamiento: El profesor López guía a los estudiantes en una comprensión profunda de cada pieza, abordando su contexto histórico, el estilo del compositor y la esencia dramática de la obra.
             <br></br>
             <br></br>
                 Con una trayectoria que incluye escenarios de prestigio como el Salón Dorado del Teatro Colón, su enfoque fomenta un aprendizaje integral. En este espacio, la técnica vocal se armoniza con la sensibilidad musical y el conocimiento académico, brindando al intérprete las herramientas necesarias para abordar el gran repertorio lírico con solvencia y madurez artística.
