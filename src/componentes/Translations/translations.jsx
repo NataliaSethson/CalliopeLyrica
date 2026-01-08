8d2bfa234747dc5a8b7fab746f4ba17d24c5f5fa
Natalia Sethson <nataliasethson@live.com.ar>
2026-01-08 08:57:20 -0300

    cambios

--- src/componentes/Translations/translations.jsx
+++
@@ -1,4 +0,0 @@
-export const resources = {
-  es: { translation: { "welcome": "Bienvenidos a la Ópera", "inst": "INSTITUCIONAL" } },
-  it: { translation: { "welcome": "Benvenuti al l'Opera", "inst": "ISTITUZIONALE" } }
-};
